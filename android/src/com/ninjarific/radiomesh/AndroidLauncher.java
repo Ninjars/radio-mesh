f722fdd33c260f21228745f9debf6fac07d85ff7
Jeremy Stevens <jeremy@touchsurgery.com>
2017-10-30 21:12:36 +0000

    initial commit of basic libgdx project

---
+++ android/src/com/ninjarific/radiomesh/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package com.ninjarific.radiomesh;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useGyroscope = false;
+		config.useImmersiveMode = true;
+		config.useRotationVectorSensor = false;
+		initialize(new RadioMeshGame(), config);
+	}
+}
