aa93d780c6a49e6b8b9ceec10f99db182668b998
Jeremy Stevens <jeremy@touchsurgery.com>
2017-11-06 20:12:00 +0000

    fixed NodeActor draw logic to centre graphic on coordinate and so avoid unexpected offset

--- core/src/com/ninjarific/radiomesh/scene/NodeActor.java
+++ core/src/com/ninjarific/radiomesh/scene/NodeActor.java
@@ -9,7 +9,8 @@ import com.ninjarific.radiomesh.nodes.ForceConnectedNode;
 import java.util.Random;
 
 public class NodeActor extends Actor {
-    private static final Random colorRandom = new Random();
+    private static final Random COLOR_RANDOM = new Random();
+    private static final float TEXTURE_SIZE = 3;
     private final ForceConnectedNode node;
     private final Texture texture;
 
@@ -19,14 +20,15 @@ public class NodeActor extends Actor {
     }
 
     private Texture createTexture() {
-        Pixmap pixmap = new Pixmap(3, 3, Pixmap.Format.RGBA8888);
-        pixmap.setColor(colorRandom.nextFloat(), colorRandom.nextFloat(), colorRandom.nextFloat(), 1);
-        pixmap.fillCircle(1, 1, 1);
+        Pixmap pixmap = new Pixmap((int) TEXTURE_SIZE, (int) TEXTURE_SIZE, Pixmap.Format.RGBA8888);
+        pixmap.setColor(COLOR_RANDOM.nextFloat(), COLOR_RANDOM.nextFloat(), COLOR_RANDOM.nextFloat(), 1);
+        int size = (int) Math.floor(TEXTURE_SIZE/2f);
+        pixmap.fillCircle(size, size, size);
         return new Texture(pixmap);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        batch.draw(texture, node.getX(), node.getY());
+        batch.draw(texture, node.getX() - TEXTURE_SIZE/2f, node.getY() - TEXTURE_SIZE/2f);
     }
 }
