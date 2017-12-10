a354fc3163669ccaaec4958afab86dcd94f8e34d
jez <jeremy+github@jeremystevens.net>
2017-12-10 17:19:08 +0000

    implemented touch handling in RadioMeshGame to allow navigating back from WorldScreen to ScanScreen

--- core/src/com/ninjarific/radiomesh/RadioMeshGame.java
+++ core/src/com/ninjarific/radiomesh/RadioMeshGame.java
@@ -3,7 +3,9 @@ package com.ninjarific.radiomesh;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.ninjarific.radiomesh.scan.ScanScreen;
 import com.ninjarific.radiomesh.scan.radialgraph.NodeData;
 import com.ninjarific.radiomesh.world.WorldScreen;
@@ -11,7 +13,7 @@ import com.ninjarific.radiomesh.world.data.WorldModel;
 
 import java.util.List;
 
-public class RadioMeshGame extends Game {
+public class RadioMeshGame extends Game implements InputProcessor {
     private static final String TAG = RadioMeshGame.class.getSimpleName();
     private ScanScreen scanScreen;
     private WorldScreen worldScreen;
@@ -21,7 +23,7 @@ public class RadioMeshGame extends Game {
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
         Gdx.gl.glClearColor(0.24f, 0.24f, 0.24f, 1);
-
+        inputMultiplexer.addProcessor(this);
         scanScreen = new ScanScreen(this);
         worldScreen = new WorldScreen(this);
         showScanScreen();
@@ -33,6 +35,7 @@ public class RadioMeshGame extends Game {
         setScreen(scanScreen);
         scanScreen.attachToInput(inputMultiplexer);
         Gdx.input.setInputProcessor(inputMultiplexer);
+        Gdx.input.setCatchBackKey(false);
     }
 
     private void showWorldScreen(WorldModel model) {
@@ -42,6 +45,7 @@ public class RadioMeshGame extends Game {
         setScreen(worldScreen);
         worldScreen.attachToInput(inputMultiplexer);
         Gdx.input.setInputProcessor(inputMultiplexer);
+        Gdx.input.setCatchBackKey(true);
     }
 
     @Override
@@ -74,5 +78,54 @@ public class RadioMeshGame extends Game {
 
     public void onNodeSelected(NodeData data) {
         showWorldScreen(new WorldModel(data));
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.BACK){
+            // Do back button handling (show pause menu?)
+            if (getScreen() == scanScreen) {
+                Gdx.app.exit();
+            } else {
+                showScanScreen();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
