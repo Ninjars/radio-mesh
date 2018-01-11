fbd36214338a88f72bf6a6bbde02fef92accaa47
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2018-01-11 17:31:47 +0000

    wip edges

--- core/src/com/ninjarific/radiomesh/world/scene/MapActor.java
+++ core/src/com/ninjarific/radiomesh/world/scene/MapActor.java
@@ -17,12 +17,17 @@ import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.utils.ShortArray;
 import com.ninjarific.radiomesh.Constants;
 import com.ninjarific.radiomesh.coordinates.Bounds;
+import com.ninjarific.radiomesh.coordinates.Coordinate;
 import com.ninjarific.radiomesh.world.WorldColors;
 import com.ninjarific.radiomesh.world.data.Center;
+import com.ninjarific.radiomesh.world.data.Corner;
 import com.ninjarific.radiomesh.world.data.Edge;
 import com.ninjarific.radiomesh.world.data.MapProperties;
 
+import org.kynosarges.tektosyne.geometry.PointD;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -47,7 +52,6 @@ public class MapActor extends Actor {
         setX((float) bounds.left);
         setY((float) bounds.top);
         setBounds(getX(), getY(), getWidth(), getHeight());
-
     }
 
     @Override
@@ -71,6 +75,70 @@ public class MapActor extends Actor {
             Color color = getColorForMapProperties(colorRandom, center.getMapProperties());
             outList.add(createSprite(center.getBounds(), center.getPolygon(), color));
         }
+        for (Edge edge : edges) {
+            Color color = WorldColors.getEdgeColor(edge.getRiverValue());
+            outList.add(createEdgeSprite(edge.v0, edge.v1, color));
+        }
+    }
+
+    private static PolygonSprite createEdgeSprite(Corner a, Corner b, Color color) {
+        final double riverFactor = 0.5;
+        List<Coordinate> cornerCoordinates = Arrays.asList(
+                getPerpendicularPoint(a.position, b.position, a.getMapProperties().getRiverValue() * riverFactor),
+                getPerpendicularPoint(b.position, a.position, a.getMapProperties().getRiverValue() * riverFactor),
+                getPerpendicularPoint(a.position, b.position, -a.getMapProperties().getRiverValue() * riverFactor),
+                getPerpendicularPoint(b.position, a.position, -a.getMapProperties().getRiverValue() * riverFactor));
+        double minX, minY, maxX, maxY, dx, dy;
+        minX = minY = Float.MAX_VALUE;
+        maxX = maxY = Float.MIN_VALUE;
+        for (Coordinate coord : cornerCoordinates) {
+            if (minX > coord.x) {
+                minX = coord.x;
+            }
+            if (maxX < coord.x) {
+                maxX = coord.x;
+            }
+            if (minY > coord.y) {
+                minY = coord.y;
+            }
+            if (maxY < coord.y) {
+                maxY = coord.y;
+            }
+        }
+        dx = maxX - minX;
+        dy = maxY - minY;
+
+        Pixmap pixmap = new Pixmap((int) Math.ceil(dx), (int) Math.ceil(dy), Pixmap.Format.RGBA8888);
+        pixmap.setColor(color);
+        pixmap.fill();
+
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose();
+        float[] vertices = new float[] {
+                (float)cornerCoordinates.get(0).x, (float)cornerCoordinates.get(0).y,
+                (float)cornerCoordinates.get(1).x, (float)cornerCoordinates.get(1).y,
+                (float)cornerCoordinates.get(2).x, (float)cornerCoordinates.get(2).y,
+                (float)cornerCoordinates.get(3).x, (float)cornerCoordinates.get(3).y,
+        };
+        short[] triangles = new short[] {
+                0, 1, 2,
+                1, 2, 3
+        };
+
+        PolygonRegion region = new PolygonRegion(new TextureRegion(texture), vertices, triangles);
+        PolygonSprite sprite = new PolygonSprite(region);
+        sprite.setOrigin((float) minX, (float) minY);
+
+        return sprite;
+    }
+
+    private static Coordinate getPerpendicularPoint(Coordinate start, Coordinate stop, double distance) {
+        double x = -start.y - stop.y;
+        double y = start.x - stop.x;
+        double length = Math.sqrt((x * x) + (y * y));
+        x /= length;
+        y /= length;
+        return new Coordinate(start.x + (distance * x), start.y + (distance * y));
     }
 
     private static PolygonSprite createSprite(Rectangle bounds, Polygon polygon, Color color) {
